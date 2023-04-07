460a1d93cf4a4d1d9dfcb367962eb99a4b9b0d67
Carl Merrigan <carl.merrigan@fulbrightmail.org>
2023-04-07 00:27:44 -0500

    sgd method complete, let's hope it works

--- lstm_network1.py
+++ lstm_network1.py
@@ -9,8 +9,6 @@ class LSTM(object):
         self.seq_length=seq_length  # number of time steps to unroll the LSTM
         self.hidden_size=hidden_size # size of the hidden layer inside the LSTM
 
-        self.loss = 0
-
         self.hprev = np.zeros((self.hidden_size,1))
         self.cprev = np.zeros((self.hidden_size,1))
 
@@ -62,26 +60,41 @@ class LSTM(object):
 
         self.weight_initializer()
         self.n_steps=0
+        
+        self.losses = []
+
+        smooth_loss = -np.log(1.0/self.vocab_size)*self.seq_length
+
+        self.losses.append(smooth_loss)
     
         self.char_to_ix = { ch:i for i,ch in enumerate(chars) }
         self.ix_to_char = { i:ch for i,ch in enumerate(chars) }
         
         n=0
 
-        while n < 2:
+        batch_generator = self.get_mini_batch(training_data,mini_batch_size)
 
-            batch_generator = self.get_mini_batch(training_data,mini_batch_size)
+        current_loss = 0
+
+        while n < 2:
 
             inputs,targets = next(batch_generator)
 
-            print("Inputs: ",inputs)
-            print("Targets: ",targets)
+            current_loss = self.sgd_step(inputs,targets,mini_batch_size,learning_rate)
+
+            smooth_loss = smooth_loss*0.999 + current_loss*0.001
+
+            self.losses.append(smooth_loss)
+
+            n+=1
 
     def get_mini_batch(self,data,mini_batch_size):
 
         p = 0
 
-        while p+self.seq_length+1<len(data):
+        batch_character_size = mini_batch_size*(self.seq_length)
+
+        while (p+batch_character_size+1)<len(data):
 
             inputs,targets = [],[]
 
@@ -92,8 +105,173 @@ class LSTM(object):
 
                 p += self.seq_length
 
+            print(p)
+
             yield inputs,targets
+
+    def sgd_step(self,inputs,targets,mini_batch_size,learning_rate):
+        '''
+        Forward pass, backward pass, and gradient update for single minibatch of fixed length sequences
+        and target sequences
+
+        inputs -- list of mini_batch_size lists , each of seq_length character indices
+        targets -- same size as inputs, lists are shifted by +1
+        '''
+
+        # unpack params
+
+        Wxc = self.params['Wxc']
+        Wxu = self.params['Wxu']
+        Wxf = self.params['Wxf']
+        Wxo = self.params['Wxo']
+
+        Whc = self.params['Whc']
+        Whu = self.params['Whu']
+        Whf = self.params['Whf']
+        Who = self.params['Who']
+
+        bc = self.params['bc']
+        bu = self.params['bu']
+        bf = self.params['bf']
+        bo = self.params['bo']
+
+        # caches for forward pass
+
+        xs, hs, cs, c_tildes = {}, {}, {}, {}
+
+        gamma_us, gamma_fs, gamma_os = {}, {}, {}
+
+        ys, ps = {}, {}
+
+        inputs = np.array(inputs)
+        targets = np.array(targets)
+
+        assert(inputs.shape==(mini_batch_size,self.seq_length))
+        assert(targets.shape==(mini_batch_size,self.seq_length))
+
+        hs[-1] = self.hprev
+        cs[-1] = self.cprev
+
+        loss = 0
         
+        for t in range(self.seq_length):
+            xs[t] = np.zeros((self.vocab_size,mini_batch_size))
+
+            xs[t][inputs[:,t],np.arange(mini_batch_size)]=1  # batch of one-hot vectors for time t 
+
+            zc = np.dot(Wxc,xs[t]) + np.dot(Whc,hs[t-1]) + bc  # linear activation for candidate cell state C~
+            zu = np.dot(Wxu,xs[t]) + np.dot(Whu,hs[t-1]) + bu  # linear activation for update gate
+            zf = np.dot(Wxf,xs[t]) + np.dot(Whf,hs[t-1]) + bf  # linear activation for forget gate
+            zo = np.dot(Wxo,xs[t]) + np.dot(Who,hs[t-1]) + bo  # linear activation for output gate
+
+            c_tildes[t] = np.tanh(zc) # canidate for new c state
+
+            gamma_us[t] = sigmoid(zu)
+            gamma_fs[t] = sigmoid(zf)
+            gamma_os[t] = sigmoid(zo)
+
+            cs[t] = np.tanh(np.multiply(c_tildes[t],gamma_us[t]) + np.multiply(cs[t-1],gamma_fs[t]))  # tanh here is import!!!
+
+            hs[t] = np.multiply(cs[t],gamma_os[t]) # hidden state
+
+            ys[t] = np.dot(Why, hs[t]) + by # unnormalized log probabilities for next chars
+
+            ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
+
+            loss += -np.log(ps[t][targets[:,t],np.arange(mini_batch_size)])   #  ps[t] should be shape (vocab_size,mini_batch_size)
+    
+        #initialize gradients to zero
+
+        dWxc,dWxu,dWxf,dWxo = np.zeros_like(Wxc), np.zeros_like(Wxu), np.zeros_like(Wxf), np.zeros_like(Wxo)
+        dWhc,dWhu,dWhf,dWho = np.zeros_like(Whc), np.zeros_like(Whu), np.zeros_like(Whf), np.zeros_like(Who)
+        dbc,dbu,dbf,dbo = np.zeros_like(bc), np.zeros_like(bu), np.zeros_like(bf), np.zeros_like(bo)
+        dWhy,dby = np.zeros_like(Why), np.zeros_like(by)
+
+        # tmp variables to accumulate gradients over the backprop -- see differentiation graph  
+        dhnext, dcnext = np.zeros_like(hs[0]), np.zeros_like(cs[0])
+
+        #backward pass
+        
+        for t in reversed(range(len(inputs))):
+            dy = np.copy(ps[t])
+            dy[targets[:,t],np.arange(mini_batch_size)] -= 1 # backprop into y
+
+            dWhy += np.dot(dy, hs[t].T)
+            dby += dy
+
+            dh = np.dot(Why.T, dy) + dhnext # backprop into h
+
+            dc = np.multiply((1-cs[t]**2),np.multiply(gamma_os[t],dh) + dcnext) #backprop into c
+
+            dcnext = np.multiply(gamma_fs[t],dc)
+
+            dzc = np.multiply((1-c_tildes[t]**2),np.multiply(gamma_us[t],dc))  # backprop through tanh
+
+            dzu = np.multiply(gamma_us[t]*(1-gamma_us[t]),np.multiply(c_tildes[t],dc))  # sigmoid prime
+
+            dzf = np.multiply(gamma_fs[t]*(1-gamma_fs[t]),np.multiply(cs[t-1],dc))
+
+            dzo = np.multiply(gamma_os[t]*(1-gamma_os[t]),np.multiply(cs[t],dh))
+
+            dbc += dzc
+            dbu += dzu
+            dbf += dzf
+            dbo += dzo
+
+            dWhc += np.dot(dzc,hs[t-1].T)
+            dWhu += np.dot(dzu,hs[t-1].T)
+            dWhf += np.dot(dzf,hs[t-1].T)
+            dWho += np.dot(dzo,hs[t-1].T)
+
+            dWxc += np.dot(dzc,xs[t].T)
+            dWxu += np.dot(dzu,xs[t].T)
+            dWxf += np.dot(dzf,xs[t].T)
+            dWxo += np.dot(dzo,xs[t].T)
+
+            # four contributions to dhnext,one from each gate
+            dhnext = np.dot(Whc.T,dzc) + np.dot(Whu.T,dzu) + np.dot(Whf.T,dzf) + np.dot(Who.T,dzo)
+        
+        grads = dict()
+
+        grads['Wxc']=dWxc
+        grads['Wxu']=dWxu
+        grads['Wxf']=dWxf
+        grads['Wxo']=dWxo
+
+        grads['Whc']=dWhc
+        grads['Whu']=dWhu
+        grads['Whf']=dWhf
+        grads['Who']=dWho
+
+        grads['bc']=dbc
+        grads['bu']=dbu
+        grads['bf']=dbf
+        grads['bo']=dbo
+
+        grads['Why']=dWhy
+        grads['dby']=dby
+
+        for parameter in grads.keys():
+            np.clip(grads[parameter], -5, 5, out=grads[parameter]) # clip to mitigate exploding gradients
+
+        self.grads=grads
+
+        self.hprev = hs[self.seq_length-1]
+        self.cprev = cs[self.seq_length-1]
+
+        # perform parameter update with Adagrad
+        for parameter in self.params.keys():
+            
+            dparam = self.grads[parameter]
+
+            self.mems[parameter] += dparam * dparam
+
+            mem = self.mems[parameter]
+            
+            self.params[parameter] += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
+
+        return loss
+
 
 if __name__ == '__main__':
 
@@ -105,7 +283,7 @@ if __name__ == '__main__':
 
     print(type(lstm),lstm.seq_length,lstm.hidden_size)
 
-    training_data = 'abcdefghijklmnopqrstuv'
+    training_data = 'abcdefghijklmnopqrstuvwxyz'*10
 
     lstm.stochastic_gradient_descent(training_data=training_data,mini_batch_size=2,learning_rate=0.001)
 
